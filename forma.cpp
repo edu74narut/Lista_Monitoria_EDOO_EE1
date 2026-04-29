a799b4491cc3e5fdacef0efa9917c04c5159cf02
Eduardo Feliciano <eduardofeliciano74@gmail.com>
2026-04-28 23:22:23 -0300

    fix: Adicionando método destrutor faltante em forma.cpp

--- forma.cpp
+++ forma.cpp
@@ -3,6 +3,9 @@
 Forma::Forma(const std::string& nome){
     this->nome = nome;
 }
+
+Forma::~Forma(){};
+
 std::string Forma::descricao() const{
     return "[" + nome + "] | Area: " + std::to_string(area()) + " | Perimetro: " + std::to_string(perimetro());
 }
